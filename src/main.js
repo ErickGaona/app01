4b4b04da8f4e8a53b2c187cd0eb75340819fdeb6
ErickGaona <erickgaona625@gmail.com>
2025-04-29 09:50:46 -0500

    actualizando

--- src/main.js
+++ src/main.js
@@ -1,24 +1,25 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import $ from 'jquery';
+import dt from 'datatables.net-dt';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+$(document).ready(function () {
+    const mi_tabla = $('#tabla-ejemplo').DataTable();
+    var url = "https://pkgstore.datahub.io/core/continent-codes/continent-codes_json/data/60d6baef1250bc2b01fd0148dccca518/continent-codes_json.json"
 
-setupCounter(document.querySelector('#counter'))
+
+    $("#obtenerInformacion").click(function () {
+        $.ajax({
+            dataType: 'json',
+            url: url
+        }).then(function (data) {
+            data.forEach(function (item) {
+                mi_tabla.row.add([
+                    item.Code,
+                    item.Name
+                ]).draw(false);
+            });
+        });
+    });
+    $("#limpiarInformacion").click(function () {
+        mi_tabla.clear().draw();
+    });
+});
